487d12c295f2a722b361e28ede811267517e1b43
Zan Spiler <zan.spiler@celtra.com>
2024-04-20 12:05:33 +0200

    initial commit

---
+++ example/d2sc_policy_example.dart
@@ -0,0 +1,6 @@
+import 'package:d2sc_policy/d2sc_policy.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/d2sc_policy_test.dart
@@ -0,0 +1,16 @@
+import 'package:d2sc_policy/d2sc_policy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
